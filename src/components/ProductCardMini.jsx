a0359f6116fba06b0a58c6d00702b05b482298cb
vitviki <vtyagi1991@gmail.com>
2024-09-20 17:05:03 +0530

    implement wishlist

--- src/components/ProductCardMini.jsx
+++ src/components/ProductCardMini.jsx
@@ -1,17 +1,74 @@
 import { Link } from "react-router-dom";
+import { IoMdHeart, IoIosHeartEmpty } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { setWishlist } from "../redux/features/user/userSlice";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 const ProductCardMini = ({ id, title, photo, badge, price }) => {
+  const dispatch = useDispatch();
+  const { user, wishlist } = useSelector((state) => state.user);
+  const [isInWishlist, setIsInWishlist] = useState(false);
+
+  const addToWishlist = (id) => {
+    // Check if the user is logged in or not.
+    if (!user) {
+      toast.info("Please login to add to wishlist");
+      return;
+    }
+
+    // Check if the product is already in the wishlist. If yes, remove it.
+    let newWishlist = [];
+    if (wishlist.includes(id)) {
+      newWishlist = wishlist.filter((item) => item !== id);
+    } else {
+      // Else add to wishlist.
+      newWishlist = [...wishlist, id];
+    }
+
+    // Update the wisthlist
+    dispatch(setWishlist(newWishlist));
+  };
+
+  useEffect(() => {
+    // Check if the the item is in wishlist.
+    if (wishlist.includes(id)) {
+      setIsInWishlist(true);
+    } else {
+      setIsInWishlist(false);
+    }
+  }, [wishlist]);
+
   return (
-    <Link to={`products/${id}`}>
+    <div className="group">
       {badge && (
         <div className="absolute top-0 left-0 flex items-center px-3 bg-red-500 text-white font-medium md:text-base text-sm">
           <h5>{badge}</h5>
         </div>
       )}
-      <img src={photo} alt={title} className="md:mt-3 mt-2" />
+      <div className="absolute top-0 right-0 text-xl hidden group-hover:block">
+        <div className="p-2 bg-gray-100 rounded-full">
+          {isInWishlist ? (
+            <IoMdHeart
+              className=" text-red-600 cursor-pointer "
+              title={isInWishlist ? "Remove from wishlist" : "Add to wishlist"}
+              onClick={() => addToWishlist(id)}
+            />
+          ) : (
+            <IoIosHeartEmpty
+              className="text-gray-600 cursor-pointer "
+              title="Add to wishlist"
+              onClick={() => addToWishlist(id)}
+            />
+          )}
+        </div>
+      </div>
+      <Link className="md:mt-3 mt-2" to={`products/${id}`}>
+        <img src={photo} alt={title} />
+      </Link>
       <h3 className="md:text-sm text-xs font-medium truncate ...">{title}</h3>
       {price && <p className="text-center text-sm text-gray-900">{price}</p>}
-    </Link>
+    </div>
   );
 };
 
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,39 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { sampleDeals, sampleBestSellers } from "../utils/data";
+
+const WishlistItem = () => {};
+
 const Wishlist = () => {
-  return <div className="w-full h-screen">Wishlist</div>;
+  const navigate = useNavigate();
+  const { wishlist } = useSelector((state) => state.user);
+
+  return (
+    <div className="w-full h-screen">
+      {wishlist.length === 0 ? (
+        <div className="w-full h-max flex flex-col items-center gap-10 mt-40 sm:px-0 px-3">
+          <h1 className="sm:text-3xl text-2xl text-center">
+            You haven't saved anything yet
+          </h1>
+          <h3 className=" text-center">
+            Add your favorites to wishlist and start building your personal
+            collection
+          </h3>
+          <button
+            onClick={() => navigate("/")}
+            className="min-w-[200px] sm:w-[300px] w-[200px] border py-3 bg-amazon_yellow sm:text-xl text-base shadow-lg"
+          >
+            Shop Now
+          </button>
+        </div>
+      ) : (
+        <div className="w-full h-max flex flex-col gap-10 mt-20 px-10">
+          <h2>My wishlist: {wishlist.length} item(s)</h2>
+          <div className="w-full grid grid-cols-5 gap-5"></div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Wishlist;
