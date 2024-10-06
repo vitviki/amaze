c72c32daaa18240ce53fabbb46f63cba556d01f3
vitviki <vtyagi1991@gmail.com>
2024-10-06 20:20:27 +0530

    removed access control for specific urls

--- backend/admin_backend/index.js
+++ backend/admin_backend/index.js
@@ -17,14 +17,6 @@ app.use(
     credentials: true,
   })
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 // Routes
 app.use("/api/products", productRoute);
