844a28e66d18e51d28dae02a3bc29238861002ea
vitviki <vtyagi1991@gmail.com>
2024-11-11 20:10:39 +0530

    fixed cors import error

--- backend/admin_backend/index.js
+++ backend/admin_backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors";
 import productRoute from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
