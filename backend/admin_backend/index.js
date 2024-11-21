e7db968e70f42f6374ef091121253d0c9bb20341
vitviki <vtyagi1991@gmail.com>
2024-11-21 12:40:38 +0530

    changed CORS origin to allow all URLS

--- backend/admin_backend/index.js
+++ backend/admin_backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import mongoose from "mongoose";
 import productRoute from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
@@ -10,19 +11,11 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "OX-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Routes
 app.get("/", (req, res) => {
